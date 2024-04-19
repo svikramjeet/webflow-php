0a194beb1cf47bfedd549119e14daf43d2d1fa1e
Vikramjeet Singh <vikramjeet@founderandlightning.com>
2024-04-19 10:36:41 +0530

    Update namespace

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,20 +1,16 @@
 <?php
 
-namespace Vikramjeet Singh\Webflow\Tests;
+namespace Svikramjeet\Webflow\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Vikramjeet Singh\Webflow\WebflowServiceProvider;
+use Svikramjeet\Webflow\WebflowServiceProvider;
 
 class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Vikramjeet Singh\\Webflow\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
